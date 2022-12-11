dfc30da381879d756301f5cb70dc8d8ea70d185f
OlgaSnopko <snopkoou@gmail.com>
2022-12-11 03:13:39 +0300

    1

---
+++ HW1.py
@@ -0,0 +1,42 @@
+# Exercise 1 Создать переменную типа String.
+
+# Exercise 2 Создать переменную типа Integer.
+
+# Exercise 3 Создать переменную типа Float.
+
+# Exercise 4 Создать переменную типа Bytes.
+
+# Exercise 5 Создать переменную типа List.
+
+# Exercise 6 Создать переменную типа Tuple.
+
+# Exercise 7 Создать переменную типа Set.
+
+# Exercise 8 Создать переменную типа Frozen set.
+
+# Exercise 9 Создать переменную типа Dict.
+
+# Exercise 10 Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
+
+# Exercise 11 Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные.
+# Вывести в консоль.
+
+# Exercise 12 Вывести в одну строку переменные типа String и Integer используя “,” (Запятую).
+a = 'one'
+b = 1
+print(a, ",", b, sep="")
+print(a, ",", 1, sep="")
+print(a, b, sep=",")
+print(a, 1, sep=",")
+print(f"{a},{b}")
+print(f"{a},1")
+# Exercise 13 Вывести в одну строку переменные типа String и Integer используя “+” (Плюс).
+a = 'one'
+b = 1
+print(a, "+", b, sep="")
+print(a, "+", 1, sep="")
+print(a, b, sep="+")
+print(a, 1, sep="+")
+print(f"{a}+{b}")
+print(f"{a}+1")
+
